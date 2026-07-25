63a626b376a5379229fe4433b2cede21990b248d
nam <namish@tuta.io>
2026-07-25 12:25:15 +0530

    digitsn, lettersn

--- src/parser.zig
+++ src/parser.zig
@@ -73,6 +73,8 @@ pub const Parser = union(enum) {
     sequence: []const Parser,
     digits,
     letters,
+    lettersN: usize,
+    digitsN: usize,
     eof,
 
     pub fn parse(self: Parser, state: *ParserState) Error!Span {
@@ -97,6 +99,40 @@ pub const Parser = union(enum) {
                 if (state.index == start) {
                     state.record_expected("letter");
                     return error.CouldNotMatch;
+                }
+            },
+
+            .lettersN => |size| {
+                if (size > state.input.len - start) {
+                    state.record_expected(std.fmt.allocPrint(std.heap.page_allocator, "{d} letters", .{size}) catch "letters");
+                    return error.CouldNotMatch;
+                }
+
+                const end = start + size;
+                while (state.index < end) {
+                    if (!std.ascii.isAlphabetic(state.input[state.index])) {
+                        state.record_expected("letter");
+                        state.index = start;
+                        return error.CouldNotMatch;
+                    }
+                    state.index += 1;
+                }
+            },
+
+            .digitsN => |size| {
+                if (size > state.input.len - start) {
+                    state.record_expected(std.fmt.allocPrint(std.heap.page_allocator, "{d} digits", .{size}) catch "digits");
+                    return error.CouldNotMatch;
+                }
+
+                const end = start + size;
+                while (state.index < end) {
+                    if (!std.ascii.isDigit(state.input[state.index])) {
+                        state.record_expected("digit");
+                        state.index = start;
+                        return error.CouldNotMatch;
+                    }
+                    state.index += 1;
                 }
             },
 
@@ -168,6 +204,14 @@ pub fn letters() Parser {
 
 pub fn eof() Parser {
     return .eof;
+}
+
+pub fn digitsN(n: usize) Parser {
+    return .{ .digitsN = n };
+}
+
+pub fn lettersN(n: usize) Parser {
+    return .{ .lettersN = n };
 }
 
 test "string parser" {
@@ -273,6 +317,96 @@ test "failure sequence with strings and eof" {
             try testing.expectEqual(@as(usize, 11), e.index);
             try testing.expectEqualStrings("end of input", e.expected);
             try testing.expectEqualStrings(" wow", e.found);
+        },
+    }
+}
+
+test "letters" {
+    const parser = letters();
+    switch (parser.run("helloworld")) {
+        .success => |result| {
+            try testing.expectEqualStrings("helloworld", result.value());
+        },
+        .err => |_| {
+            try testing.expect(false);
+        },
+    }
+}
+
+test "lettersN success" {
+    const parser = lettersN(5);
+    switch (parser.run("helloworld")) {
+        .success => |result| {
+            try testing.expectEqualStrings("hello", result.value());
+            try testing.expectEqual(@as(usize, 5), result.consumed);
+        },
+        .err => |_| try testing.expect(false),
+    }
+}
+
+test "lettersN overflow" {
+    const parser = lettersN(15);
+    switch (parser.run("helloworld")) {
+        .success => |_| try testing.expect(false),
+        .err => |e| {
+            try testing.expectEqual(@as(usize, 0), e.index);
+            try testing.expect(std.mem.startsWith(u8, e.expected, "15"));
+        },
+    }
+}
+
+test "lettersN not enough letters" {
+    const parser = lettersN(5);
+    switch (parser.run("hel12")) {
+        .success => |_| try testing.expect(false),
+        .err => |e| {
+            try testing.expectEqual(@as(usize, 3), e.index);
+            try testing.expectEqualStrings("letter", e.expected);
+        },
+    }
+}
+
+test "digits" {
+    const parser = digits();
+    switch (parser.run("12345654321")) {
+        .success => |result| {
+            try testing.expectEqualStrings("12345654321", result.value());
+        },
+        .err => |_| {
+            try testing.expect(false);
+        },
+    }
+}
+
+test "digitsN success" {
+    const parser = digitsN(6);
+    switch (parser.run("123123")) {
+        .success => |result| {
+            try testing.expectEqualStrings("123123", result.value());
+            try testing.expectEqual(@as(usize, 6), result.consumed);
+        },
+        .err => |_| try testing.expect(false),
+    }
+}
+
+test "digitsN overflow" {
+    const parser = digitsN(15);
+    switch (parser.run("12")) {
+        .success => |_| try testing.expect(false),
+        .err => |e| {
+            try testing.expectEqual(@as(usize, 0), e.index);
+            try testing.expect(std.mem.startsWith(u8, e.expected, "15"));
+        },
+    }
+}
+
+test "digitsN not enough letters" {
+    const parser = digitsN(5);
+    switch (parser.run("123AB")) {
+        .success => |_| try testing.expect(false),
+        .err => |e| {
+            try testing.expectEqual(@as(usize, 3), e.index);
+            try testing.expectEqualStrings("digit", e.expected);
         },
     }
 }
